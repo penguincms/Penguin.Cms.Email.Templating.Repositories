64f94d460cce320e21a24c46a10a8edad5e885c7
anloffredo <anloffredo@massageenvy.com>
2020-05-14 19:25:42 -0700

    Auto Commit

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -4,5 +4,4 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>", Scope = "member", Target = "~M:Penguin.Cms.Email.Templating.Repositories.StackInformation.ValidateMethodParameters(System.Collections.Generic.List{Penguin.Templating.Abstractions.TemplateParameter})")]
-
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1710:Identifiers should have correct suffix", Justification = "<Pending>", Scope = "type", Target = "~T:Penguin.Cms.Email.Templating.Repositories.EmailTemplateRepository")]
